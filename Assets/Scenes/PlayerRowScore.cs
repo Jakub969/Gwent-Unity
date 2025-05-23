09bc5dd98f152c1218e9831da810bfe727d34d8c
Jakub969 <jakub.hrubizna@gmail.com>
2025-05-23 18:00:17 +0200

    Pridanie počítania skore na riadkoch

---
+++ Assets/Scenes/PlayerRowScore.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerRowScore : MonoBehaviour
+{
+    public TMP_Text scoreText;
+
+    void Update()
+    {
+        int total = 0;
+
+        foreach (Transform card in transform)
+        {
+            CardDisplay display = card.GetComponent<CardDisplay>();
+            if (display != null && display.cardData != null)
+            {
+                total += display.cardData.strength;
+            }
+        }
+
+        scoreText.text = total.ToString();
+    }
+}
